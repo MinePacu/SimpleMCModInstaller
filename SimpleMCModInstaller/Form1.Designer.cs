8eb1060a90fd6b3ed541e0a555da5b02f3bd334e
subin <subin@PACU_COMPUTER>
2025-01-06 20:02:19 +0900

    초기 커밋.

---
+++ SimpleMCModInstaller/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace SimpleMCModInstaller
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Add_ModFile_Button = new Button();
+            Install_Button = new Button();
+            ModList_dataGridView = new DataGridView();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            removeModButton = new Button();
+            progressBar1 = new ProgressBar();
+            InstallingFileText = new Label();
+            ((System.ComponentModel.ISupportInitialize)ModList_dataGridView).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Add_ModFile_Button
+            // 
+            Add_ModFile_Button.Location = new Point(3, 3);
+            Add_ModFile_Button.Name = "Add_ModFile_Button";
+            Add_ModFile_Button.Size = new Size(75, 23);
+            Add_ModFile_Button.TabIndex = 0;
+            Add_ModFile_Button.Text = "모드 추가";
+            Add_ModFile_Button.UseVisualStyleBackColor = true;
+            Add_ModFile_Button.Click += Add_ModFile_Button_Click;
+            // 
+            // Install_Button
+            // 
+            Install_Button.Location = new Point(165, 3);
+            Install_Button.Name = "Install_Button";
+            Install_Button.Size = new Size(75, 23);
+            Install_Button.TabIndex = 1;
+            Install_Button.Text = "설치";
+            Install_Button.UseVisualStyleBackColor = true;
+            Install_Button.Click += Install_Button_Click;
+            // 
+            // ModList_dataGridView
+            // 
+            ModList_dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            ModList_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            ModList_dataGridView.Location = new Point(3, 32);
+            ModList_dataGridView.Name = "ModList_dataGridView";
+            ModList_dataGridView.RowTemplate.Height = 25;
+            ModList_dataGridView.Size = new Size(762, 374);
+            ModList_dataGridView.TabIndex = 2;
+            ModList_dataGridView.SelectionChanged += ModList_dataGridView_SelectionChanged;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            flowLayoutPanel1.Controls.Add(Add_ModFile_Button);
+            flowLayoutPanel1.Controls.Add(removeModButton);
+            flowLayoutPanel1.Controls.Add(Install_Button);
+            flowLayoutPanel1.Controls.Add(ModList_dataGridView);
+            flowLayoutPanel1.Controls.Add(progressBar1);
+            flowLayoutPanel1.Controls.Add(InstallingFileText);
+            flowLayoutPanel1.Location = new Point(12, 12);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(776, 467);
+            flowLayoutPanel1.TabIndex = 3;
+            // 
+            // removeModButton
+            // 
+            removeModButton.Location = new Point(84, 3);
+            removeModButton.Name = "removeModButton";
+            removeModButton.Size = new Size(75, 23);
+            removeModButton.TabIndex = 5;
+            removeModButton.Text = "모드 제거";
+            removeModButton.UseVisualStyleBackColor = true;
+            removeModButton.Click += removeModButton_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Anchor = AnchorStyles.None;
+            progressBar1.Location = new Point(3, 412);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(762, 23);
+            progressBar1.TabIndex = 3;
+            // 
+            // InstallingFileText
+            // 
+            InstallingFileText.AutoSize = true;
+            InstallingFileText.Location = new Point(771, 409);
+            InstallingFileText.Name = "InstallingFileText";
+            InstallingFileText.Size = new Size(0, 15);
+            InstallingFileText.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 491);
+            Controls.Add(flowLayoutPanel1);
+            Name = "MainForm";
+            SizeChanged += MainForm_SizeChanged;
+            ((System.ComponentModel.ISupportInitialize)ModList_dataGridView).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button Add_ModFile_Button;
+        private Button Install_Button;
+        private DataGridView ModList_dataGridView;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private ProgressBar progressBar1;
+        private Label InstallingFileText;
+        private Button removeModButton;
+    }
+}
---
+++ SimpleMCModInstaller/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace SimpleMCModInstaller
+{
+    public partial class MainForm : Form
+    {
+        BackgroundWorker worker;
+        List<string> modFileList = new();
+        List<string> modFileList_WithPath = new();
+        int nowfileIndex = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InstallingFileText.Text = "시작 대기 중..";
+            removeModButton.Enabled = false;
+
+            if (ModList_dataGridView.CurrentRow != null)
+                removeModButton.Enabled = true;
+
+            BindingList<filePathName> modFileList_Array = new BindingList<filePathName>() { };
+            ModList_dataGridView.DataSource = modFileList_Array;
+
+            ModList_dataGridView.Columns[0].HeaderText = "파일 이름";
+            ModList_dataGridView.Columns[1].HeaderText = "파일 경로";
+        }
+
+        private void Add_ModFile_Button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new();
+            dialog.Filter = "jar 파일 (*.jar)|*.jar|모든 파일 (*.*)|*.*";
+
+            dialog.FilterIndex = 0;
+            dialog.Multiselect = true;
+
+            BindingList<filePathName>? data = ModList_dataGridView.DataSource as BindingList<filePathName>;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Debug.WriteLine("dialog.FileNames.Length : " + dialog.FileNames.Length);
+                if (dialog.FileNames.Length > 1)
+                {
+                    for (int i = 0; i < dialog.FileNames.Length; i++)
+                    {
+                        data?.Add(new filePathName { fileName = dialog.SafeFileNames[i], filePath = dialog.FileNames[i] });
+                        modFileList.Add(dialog.SafeFileNames[i]);
+                        modFileList_WithPath.Add(dialog.FileNames[i]);
+                    }
+                }
+
+                else
+                {
+                    Debug.WriteLine("SelectedFileName : " + dialog.SafeFileName);
+                    Debug.WriteLine("SelectedFilePath : " + dialog.FileName);
+
+                    data?.Add(new filePathName { fileName = dialog.SafeFileName, filePath = dialog.FileName });
+                    modFileList.Add(dialog.SafeFileName);
+                    modFileList_WithPath.Add(dialog.FileName);
+                }
+
+                dialog.Dispose();
+            }
+        }
+
+        private void Install_Button_Click(object sender, EventArgs e)
+        {
+            Add_ModFile_Button.Enabled = false;
+            Install_Button.Enabled = false;
+
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+
+            worker.RunWorkerAsync();
+        }
+
+        private void Worker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            InstallingFileText.Text = "작업 완료";
+            MessageBox.Show("모드 설치가 완료되었습니다.");
+            worker.Dispose();
+
+            Add_ModFile_Button.Enabled = true;
+            Install_Button.Enabled = true;
+            InstallingFileText.Text = "시작 대기 중..";
+        }
+
+        private void Worker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+            InstallingFileText.Text = modFileList[nowfileIndex] + " 파일 작업 중..";
+        }
+
+        private void Worker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            string minecraftmodsPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString() + "\\.minecraft\\mods\\";
+            string minecraftPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString() + "\\.minecraft\\";
+
+            Debug.WriteLine("minecraft_mods_path : " + minecraftmodsPath);
+            Debug.WriteLine("IsExistModsPath : " + Directory.Exists(minecraftmodsPath));
+
+            BindingList<filePathName>? data = ModList_dataGridView.DataSource as BindingList<filePathName>;
+
+            if (Directory.Exists(minecraftmodsPath))
+            {
+                for (int i = 0; data?.Count > i; i++)
+                {
+                    Thread.Sleep(50);
+                    File.Copy(data[i].filePath, minecraftmodsPath + "\\" + data[i].fileName);
+                    worker.ReportProgress((i + 1 / data.Count) * 100);
+                    nowfileIndex = i;
+                }
+            }
+
+            else if (Directory.Exists(minecraftPath))
+            {
+                MessageBox.Show("포지 Mod Loader가 설치되어 있지 않습니다. 재설치 후, 다시 시도하세요.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                worker.CancelAsync();
+            }
+
+            else
+            {
+                MessageBox.Show("마인크래프트가 설치되어 있지 않지 않습니다. 재설치 후, 다시 시도하세요.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                worker.CancelAsync();
+            }
+        }
+
+        private void removeModButton_Click(object sender, EventArgs e)
+        {
+            BindingList<filePathName>? data = ModList_dataGridView.DataSource as BindingList<filePathName>;
+            data?.RemoveAt(ModList_dataGridView.CurrentRow.Index);
+        }
+
+        private void ModList_dataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            var grid = (DataGridView)sender;
+            if (grid.SelectedRows.Count > 0)
+                removeModButton.Enabled = true;
+        }
+
+        private void MainForm_SizeChanged(object sender, EventArgs e)
+        {
+            int gridX = flowLayoutPanel1.Location.X;   //그리드컨트롤의 시작 X 좌표
+            int gridY = flowLayoutPanel1.Location.Y;  //그리드컨트롤의 시작 Y 좌표
+
+            flowLayoutPanel1.Width = this.Width - (gridX * 2 + 20); //보기 좋게 적당한 사이즈로 계산
+            flowLayoutPanel1.Height = this.Height - gridY - 55; // 보기 좋게 적당한 사이즈로 계산
+
+            progressBar1.Width = this.Width - (gridX * 2 + 20);
+
+            ModList_dataGridView.Width = flowLayoutPanel1.Width - 30;
+            ModList_dataGridView.Height = flowLayoutPanel1.Height - (progressBar1.Height + InstallingFileText.Height) - 50;
+        }
+    }
+}
